70a0d2097b68efecaf72fbe1448850e286f5e53b
olexy <h.lekan@gmail.com>
2017-03-09 17:55:31 +0100

    creating  the root component (app.component.ts)

--- skeleton/app/app.module.ts
+++ skeleton/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent } from './app.component'
+
 @NgModule({
-    imports: [ BrowserModule]
+    imports: [ BrowserModule],
+    declarations: [ AppComponent ], //making the component available for use
+    bootstrap: [ AppComponent ]     //the starting point of the app to load the component
 })
 
 export class AppModule { }
