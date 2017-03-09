63d3d5352bbfc7f587a4875a8116922356ad5743
olexy <h.lekan@gmail.com>
2017-03-09 18:56:07 +0100

    fix node_modules/ "npm:" alias

---
+++ skeleton/app/main.js
@@ -0,0 +1,6 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_module_1 = require('./app.module');
+var platform = platform_browser_dynamic_1.platformBrowserDynamic(); //creat an instance
+platform.bootstrapModule(app_module_1.AppModule); //Initializes the app module
+//# sourceMappingURL=main.js.map
