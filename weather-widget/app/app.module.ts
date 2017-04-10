0df27b2cc9ce76ca7a102ee876cdecf492679698
Lekan Stephen <h.lekan@gmail.com>
2017-04-10 21:52:52 +0100

    creating the weather component

--- weather-widget/app/app.module.ts
+++ weather-widget/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather-widget/component/weather.component'
 
 @NgModule({
     imports: [ BrowserModule],
-    declarations: [ AppComponent ], //making the component available for use
+    declarations: [ AppComponent, WeatherComponent ], //making the component available for use
     bootstrap: [ AppComponent ]     //the starting point of the app to load the component
 })
 
---
+++ weather-widget/app/weather-widget/component/weather.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId:module.id,
+    selector: 'weather-widget',
+    templateUrl: 'weather.component.html',
+    styleUrls: [ 'weather.component.css' ]
+})
+
+export class WeatherComponent {
+
+}
