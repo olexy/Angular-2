0cbccb226b82fbd4f491c3845fde82b85a67e41f
Lekan Stephen <h.lekan@gmail.com>
2017-04-18 19:36:30 +0100

    creating weather component service

--- weather_widget/app/widget/component/widget.component.js
+++ weather_widget/app/widget/component/widget.component.js
@@ -5,10 +5,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var widget_service_1 = require("../service/widget.service");
 var WeatherComponent = (function () {
-    function WeatherComponent() {
+    function WeatherComponent(service) {
+        this.service = service;
     }
     return WeatherComponent;
 }());
@@ -17,8 +22,10 @@ WeatherComponent = __decorate([
         moduleId: module.id,
         selector: 'widget',
         templateUrl: 'widget.component.html',
-        styleUrls: ['widget.component.css']
-    })
+        styleUrls: ['widget.component.css'],
+        providers: [widget_service_1.WeatherService]
+    }),
+    __metadata("design:paramtypes", [widget_service_1.WeatherService])
 ], WeatherComponent);
 exports.WeatherComponent = WeatherComponent;
 //# sourceMappingURL=widget.component.js.map
--- weather_widget/app/widget/component/widget.component.ts
+++ weather_widget/app/widget/component/widget.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../service/widget.service'
 
 @Component({
     moduleId:module.id,
     selector:'widget',
     templateUrl:'widget.component.html',
-    styleUrls:['widget.component.css']
+    styleUrls:['widget.component.css'],
+    providers: [ WeatherService]
+
 })
 
 export class WeatherComponent{
+    constructor(private service: WeatherService){
+        
+    }
+    
+
 
 }
